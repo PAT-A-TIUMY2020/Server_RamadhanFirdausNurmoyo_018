1609405024aa77004e3adec6462c3151e75b6869
RamadhanFN <62781418+RamadhanFN@users.noreply.github.com>
2020-10-31 14:49:59 +0700

    Add project files.

---
+++ Client_RamadhanFirdausNurmoyo_018/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_RamadhanFirdausNurmoyo_018
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, c, d, y, h;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            c = a + b;
+            d = a - b;
+            y = a * b;
+            h = a / b;
+            label7.Text = c.ToString();
+            label8.Text = d.ToString();
+            label9.Text = y.ToString();
+            label10.Text = h.ToString();
+        }
+    }
+}
---
+++ Server_RamadhanFirdausNurmoyo_018/Form1.cs
@@ -0,0 +1,46 @@
+﻿using ServiceMtk_P1_20180140018;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_RamadhanFirdausNurmoyo_018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                label2.Text = "Server ON";
+                label3.Text = "Klik OFF untuk Mematikan Server";
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
